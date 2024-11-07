bbb5ae1fa27202ba0695aa0eb3790326c9d87ae4
Bacala , Anghela Q. <145820765+mignielaquerol@users.noreply.github.com>
2024-11-07 01:13:07 -0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="wid5th=device-width, initial-scale=1">
+	<title>ubuntu server-php deployment</title>
+</head>
+
+<link href="<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">"
+<body>
+
+<p>ubuntu server-php deployment- Anghela Bacala</p>
+</body>
+
+<div>
+	<label>Firstname:</label>
+	<input type="text" name="firstname" class="form-control">
+</div>
+<div class="container">
+<label>Middle:</label>
+	<input type="text" name="firstname" class="form-control">
+</div>
+<div class="container">
+<label>Lastname:</label>
+	<input type="text" name="firstname" class="form-control">
+
+	</html>
+	
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+
